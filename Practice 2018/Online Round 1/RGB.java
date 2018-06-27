42712a435182517a745f874478aa3f6f1069d34f
Mario Talevski <mario.talevski@gmail.com>
2018-06-27 23:13:24 +0200

    Add files via upload

---
+++ Practice 2018/Online Round 1/RGB.java
@@ -0,0 +1,18 @@
+
+public class RGB {
+	
+	public static void main(String[] args) {
+		
+		int N = 2;
+        long result = 0;
+        
+        for(int i=0; i<=N; i++) {
+            result = result +  N - i + 1;
+        }
+        
+        System.out.println((int)result);		
+		
+	}
+
+
+}
